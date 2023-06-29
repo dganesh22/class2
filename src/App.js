052c326865c1d8734c447431f03a700b141d1775
dganesh22 <dganesh2022@gmail.com>
2023-06-29 12:02:10 +0530

    handler in functional component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import Ex1 from './component/Ex1'
 import Ex2 from './component/Ex2'
 import Ex3 from './component/Ex3'
 import Ex4 from './component/Ex4'
+import Ex5 from './component/Ex5'
 
 function App(props) {
   return (
     <div>
         <h1>States in React</h1>
-        <Ex4/>
+        <Ex5/>
     </div>
   )
 }
